96aa9c652caa1e5f737888ab5a3263495fe09ce5
Florian <92161528+peflor@users.noreply.github.com>
2022-05-04 09:55:09 +0200

    Fichier opérationnel avec FOR & IF ELSE

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
-<?php for ($i=0; $i <= 20; $i++) : ?>
-<?php switch($i): ?>
-<?php case ($i % 3) : ?>
-<p><?php echo $i ?> - Fizz</p>
-<hr>
-<?php break; ?>
 
-<?php case ($i % 5) : ?>
-<p><?php echo $i ?> - Buzz</p>
-<hr>
-<?php break; ?>
-<?php case ($i % 3 && $i % 5) : ?>
-<p><?php echo $i ?> - FizzBuzz</p>
-<hr>
-<?php break; ?>
-<?php default : ?>
-<p><?php echo $i ?></p>
-<hr>
-<?php endswitch ?>
+<!-- FizzBuzz avec IF -->
 
+<?php for ($i=0; $i <= 20; $i++){
+    if ($i % 3 == 0) {        
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo $i . " - FizzBuzz <hr>";
+        }else{
+            echo $i . " - Fizz <hr>";
+        }
 
-<?php endfor ?>
+    }else if ($i % 5 == 0) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+        echo $i . " - FizzBuzz <hr>";
+        }else{
+            echo $i . " - Buzz <hr>";
+        }
+    }else{
+        echo $i . "<hr>";
+    }
+}
