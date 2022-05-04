a753a77e4843e8a1edf367b43d0abceade8f85cf
Florian <92161528+peflor@users.noreply.github.com>
2022-05-04 09:27:28 +0200

    switch - syntaxe alternative -
    pas totalement ok

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
-<?php
+<?php for ($i=0; $i <= 20; $i++) : ?>
+<?php switch($i): ?>
+<?php case ($i % 3) : ?>
+<p><?php echo $i ?> - Fizz</p>
+<hr>
+<?php break; ?>
+
+<?php case ($i % 5) : ?>
+<p><?php echo $i ?> - Buzz</p>
+<hr>
+<?php break; ?>
+<?php case ($i % 3 && $i % 5) : ?>
+<p><?php echo $i ?> - FizzBuzz</p>
+<hr>
+<?php break; ?>
+<?php default : ?>
+<p><?php echo $i ?></p>
+<hr>
+<?php endswitch ?>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+<?php endfor ?>
