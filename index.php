7280095a4a0ee0e1dff648a5d3d0248ab5242a2d
Florian <92161528+peflor@users.noreply.github.com>
2022-05-04 11:00:07 +0200

    code plus léger

--- index.php
+++ index.php
@@ -19,3 +19,17 @@
         echo $i . "<hr>";
     }
 }
+
+//  ----- Version Kevin ----------
+for ($i=0; $i <= 100; $i++) {
+    if (($i % 3 === 0) && ($i % 5 === 0)) {
+      echo " $i - FizzBuzz <br>";
+      } else if ($i % 3 === 0) {
+        echo " $i - Fizz <br>";
+        } else if ($i % 5 === 0) {
+          echo " $i - Buzz <br>";
+          } else {
+            echo " " . $i . " <br>";
+          }
+  }
+  
