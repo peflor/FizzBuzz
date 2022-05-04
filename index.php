aee717424cb5449e2fa6aa3872ee29199e7a32f8
Florian <92161528+peflor@users.noreply.github.com>
2022-05-04 10:02:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
     }else if ($i % 5 == 0) {
         if ($i % 3 == 0 && $i % 5 == 0) {
-        echo $i . " - FizzBuzz <hr>";
+            echo $i . " - FizzBuzz <hr>";
         }else{
             echo $i . " - Buzz <hr>";
         }
